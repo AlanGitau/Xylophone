69ba8a0e476a53ff9814bbb4b3cebd669147830b
Alan Gitau <lewisgitau99@gmail.com>
2024-10-29 04:46:45 +0300

    XylophoneApp

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,21 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:xylophone/main.dart';
 
-import 'package:xylophone_flutter/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Sound button tap test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(XylophoneApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify if the first button is present
+    expect(find.byType(TextButton), findsNWidgets(7));
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the first sound button
+    await tester.tap(find.byType(TextButton).first);
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Since there's no UI change, we would rely on AudioPlayer being called.
+    // For more complex testing, consider mocking the AudioPlayer to verify behavior.
   });
 }
